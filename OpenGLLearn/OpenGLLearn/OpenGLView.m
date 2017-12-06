ffc45ccfdbf30d8a6b3f1b964857a2e383822cd5
yaoyingtao <yaoyingtao@changba.com>
2017-12-06 18:47:35 +0800

    增加shader

--- OpenGLLearn/OpenGLLearn/OpenGLView.m
+++ OpenGLLearn/OpenGLLearn/OpenGLView.m
@@ -8,16 +8,32 @@
 
 #import "OpenGLView.h"
 
-static float vertices[] = {
-    -0.5f, -0.05f, -0.05f,
-    0.5f, -0.05f, 0.0f,
-    0.0f, 0.5f, 0.0f
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+const Vertex vertices[] = {
+    {{1, -1, 0}, {1, 0, 0, 1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte indices[] = {
+    0, 1, 2,
+    2, 3, 0
 };
 
 @interface OpenGLView ()
 @property (nonatomic, strong) CAEAGLLayer *eaglLayer;
 @property (nonatomic, strong) EAGLContext *contex;
 @property (nonatomic, assign) GLuint colorRenderBuffer;
+
+@property (nonatomic, assign) GLuint positionSlot;
+@property (nonatomic, assign) GLuint colorSlot;
+
+
 
 @end
 
@@ -30,6 +46,8 @@ static float vertices[] = {
         [self setupContex];
         [self setupRenderBuffer];
         [self setupFrameBuffer];
+        [self compleShader];
+        [self setupVBO];
         [self render];
     }
     
@@ -65,11 +83,16 @@ static float vertices[] = {
     [_contex renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
 }
 
-- (void)setupArrayBuffer {
+- (void)setupVBO {
     GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 - (void)setupFrameBuffer {
@@ -83,8 +106,78 @@ static float vertices[] = {
 - (void)render {
     glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    [_contex presentRenderbuffer:GL_RENDERBUFFER];
 
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(vertices), 0);
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(vertices), (GLvoid*)(sizeof(float)*3));
+
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(indices[0]), GL_UNSIGNED_BYTE, 0);
+
+
+    [_contex presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (void)compleShader {
+    GLuint vertexShader = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
+    GLuint fragementShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
+    
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragementShader);
+    glLinkProgram(programHandle);
+    
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    glUseProgram(programHandle);
+    
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _colorSlot = glGetAttribLocation(programHandle, "SourceColot");
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_colorSlot);
+    
+}
+
+
+
+
+
+#pragma mark - tool
+- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
+    NSString *shaderPath = [[NSBundle mainBundle] pathForResource:shaderName ofType:@"glsl"];
+    NSError *error;
+    NSString *shaderString = [NSString stringWithContentsOfFile:shaderPath encoding:NSUTF8StringEncoding error:&error];
+    if (!shaderString) {
+        NSLog(@"error loading shader");
+        exit(1);
+    }
+    
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    const char *shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = [shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    glCompileShader(shaderHandle);
+    
+    GLuint complieSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &complieSuccess);
+    if (complieSuccess == GL_FALSE) {
+        GLchar message[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(message), 0, &message[0]);
+        NSString *messageString = [NSString stringWithUTF8String:message];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    return shaderHandle;
 }
 
 
