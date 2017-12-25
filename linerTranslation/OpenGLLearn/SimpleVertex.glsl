4a5a132cd8833005db5647926438fb10b2bab011
yaoyingtao <yaoyingtao@changba.com>
2017-12-25 10:42:03 +0800

    完成线性变换

---
+++ linerTranslation/OpenGLLearn/SimpleVertex.glsl
@@ -0,0 +1,19 @@
+
+attribute vec4 Position;
+attribute vec3 SourceColor;
+attribute vec2 TexturCoord;
+
+
+uniform mat4 Projection;
+uniform mat4 Modelview;
+
+
+
+varying vec4 DestinationColor;
+varying vec2 DestTexturCoord;
+
+void main(void) {
+    DestinationColor = vec4(SourceColor, 1.0);
+    DestTexturCoord = TexturCoord;
+    gl_Position = Projection*Position;
+}
